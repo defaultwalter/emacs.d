06138b5109f0faf8a492b00f053e87660abb3550
meetcw <meetcw@outlook.com>
2021-03-29 18:36:16 +0800

    .

--- lisp/core/modal.el
+++ lisp/core/modal.el
@@ -65,26 +65,74 @@
   (modal-insert-mode -1))
 
 ;;; command
-(defun modal-forward-word (arg)
+(defun modal--set-mark()
+  (when (not (region-active-p))
+    (set-mark-command nil)))
+
+(defun modal-mark-and-forward-char(arg)
   (interactive "p")
-  (let* ((range (bounds-of-thing-at-point 'word))
-         (end (cdr range)))))
+  (call-interactively #'forward-char))
+
+(defun modal-mark-and-backward-char(arg)
+  (interactive "p")
+  (modal--set-mark)
+  (call-interactively #'backward-char))
+
+(defun modal-mark-and-forward-char(arg)
+  (interactive "p")
+  (modal--set-mark)
+  (call-interactively #'forward-char))
+
+(defun modal-mark-and-previous-line(arg)
+  (interactive "p")
+  (modal--set-mark)
+  (call-interactively #'previous-line))
+
+(defun modal-mark-and-next-line(arg)
+  (interactive "p")
+  (modal--set-mark)
+  (call-interactively #'next-line))
+
+(defun modal-mark-and-next-line(arg)
+  (interactive "p")
+  (modal--set-mark)
+  (call-interactively #'next-line))
+
+(defun modal-mark-word(arg)
+  (interactive "p")
+  (let ((point (bounds-of-thing-at-point 'word)))
+    (goto-char (cdr point))
+    (push-mark (car point) t t)))
+
+(defun modal-mark-symbol(arg)
+  (interactive "p")
+  (let ((point (bounds-of-thing-at-point 'symbol)))
+    (goto-char (cdr point))
+    (push-mark (car point) t t)))
+
+(defun modal-forward-word(arg)
+  (interactive "p")
+  (forward-thing 'word arg))
+
+(defun modal-backward-word(arg)
+  (interactive "p")
+  (let ((arg (- 0 arg)))
+    (forward-thing 'word arg)))
+
 ;;; bind map
 (define-key modal-normal-state-map (kbd "j") #'next-line)
 (define-key modal-normal-state-map (kbd "k") #'previous-line)
 (define-key modal-normal-state-map (kbd "h") #'backward-char)
 (define-key modal-normal-state-map (kbd "l") #'forward-char)
-(define-key modal-normal-state-map (kbd "L")
-  (lambda(arg)
-    (interactive "p")
-    (when (not (region-active-p))
-      (call-interactively #'set-mark-command))
-    (call-interactively #'forward-char)))
+(define-key modal-normal-state-map (kbd "L") #'modal-mark-and-forward-char)
+(define-key modal-normal-state-map (kbd "K") #'modal-mark-and-previous-line)
+(define-key modal-normal-state-map (kbd "J") #'modal-mark-and-next-line)
+(define-key modal-normal-state-map (kbd "H") #'modal-mark-and-backward-char)
 
 
-(define-key modal-normal-state-map (kbd "w") #'forward-word)
+(define-key modal-normal-state-map (kbd "w") #'modal-mark-word)
 (define-key modal-normal-state-map (kbd "W") #'backward-word)
-(define-key modal-normal-state-map (kbd "s") #'forward-symbol)
+(define-key modal-normal-state-map (kbd "s") #'modal-mark-symbol)
 (define-key modal-normal-state-map (kbd "S") #'backward-symbol)
 
 (define-key modal-normal-state-map (kbd "i") #'modal-insert-mode)
@@ -104,5 +152,5 @@ around the text moved over."
 (modal-global-mode 1)
 (cua-mode 1)
 (setq this-command-keys-shift-translated t)
-(provide 'modal)
+(provide 'core/modal)
 ;;; modal.el ends here
